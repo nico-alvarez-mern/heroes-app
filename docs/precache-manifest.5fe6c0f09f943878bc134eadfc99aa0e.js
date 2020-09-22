e2de624c287276f03c0445d26c2c2c1753260150
nicoAlvarezMern <nicolas.alvarez.mern@gmail.com>
2020-09-22 16:19:54 -0300

    s

---
+++ docs/precache-manifest.5fe6c0f09f943878bc134eadfc99aa0e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41b74ba37fc2ae997a6744435c4443d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3eea218b2979fad80515",
+    "url": "/static/js/2.b0d8f843.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b0d8f843.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04957da441a105c39e9b",
+    "url": "/static/js/main.59a8d1ba.chunk.js"
+  },
+  {
+    "revision": "94d7ca25d4df19621824",
+    "url": "/static/js/runtime-main.b7f813af.js"
+  }
+]);
---
+++ src/routers/AppRouter.js
@@ -0,0 +1,27 @@
+import React, { useContext } from 'react'
+import {
+    BrowserRouter as Router,
+    Switch
+  } from "react-router-dom";
+
+import { LoginScreen } from '../components/login/LoginScreen';
+import { DashBoardRoutes } from './DashBoardRoutes';
+import { PrivateRoute } from './PrivateRoute';
+import { AuthContext } from '../auth/AuthContext';
+import { PublicRoute } from './PublicRoute';
+
+
+export const AppRouter = () => {
+
+    const { user } = useContext(AuthContext);
+    return (
+        <Router>
+            <div>
+                <Switch>
+                    <PublicRoute exact path="/login" component={LoginScreen } isAuth={user.logged} />
+                    <PrivateRoute  path="/" component={ DashBoardRoutes } isAuth={user.logged} />
+                </Switch>
+            </div>
+        </Router>
+    )
+}
